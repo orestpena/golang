92b57cf426c56a27200658475c27119649d33567
Orest Pena <orestpena@gmail.com>
2022-06-21 01:29:07 +0000

    added solution to the error

--- pointers_in_functions2.go
+++ pointers_in_functions2.go
@@ -12,7 +12,9 @@ func main() {
 	arr := [3]int{10, 20, 30}
 	fmt.Println(arr)
 	modify(arr)
+	//modify(&arr)
 	fmt.Println(arr)
 }
 
 // this program is an error, you can't pass [3]int as type *[3]int
+// to fix this comment modify(arr) and uncomment the following line.
